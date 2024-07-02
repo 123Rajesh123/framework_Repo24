90ac4b7cf18339cd1ec5147b1c6a0c579ef28ae5
rajes <rajes@Rajeshpc>
2024-07-02 17:15:08 +0530

    First commit

---
+++ SeleniumProject/src/test/java/pack2/OrgTest.java
@@ -0,0 +1,28 @@
+package pack2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	@Test
+	public void createorgTest() {
+		System.out.println("execute createOrgTest");
+		
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
+	}
+	
+	@Test
+	public void modifyorgTest() {
+		System.out.println("execute modifyOrgTest");
+	}
+	
+}
