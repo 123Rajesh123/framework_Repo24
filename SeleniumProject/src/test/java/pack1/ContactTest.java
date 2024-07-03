e445d17e3732d02516b3ba83343385269877e180
rajes <rajes@Rajeshpc>
2024-07-03 10:40:08 +0530

    Added new test

--- SeleniumProject/src/test/java/pack1/ContactTest.java
+++ SeleniumProject/src/test/java/pack1/ContactTest.java
@@ -25,8 +25,9 @@ public class ContactTest {
 		System.out.println("execute modifycontactTest");
 	}
 	
-
-	
-	
+	@Test
+	public void deletecontactTest() {
+		System.out.println("execute deletecontactTest");
+	}	
 	
 }
